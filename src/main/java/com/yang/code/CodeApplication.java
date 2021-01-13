a9a84ebc77487f17186eda7ecfe079f15f65498a
董利洋 <15038980762@163.com>
2021-01-13 10:46:29 +0800

    springboot项目打成war包在tomcat里运行配置

--- src/main/java/com/yang/code/CodeApplication.java
+++ src/main/java/com/yang/code/CodeApplication.java
@@ -2,10 +2,19 @@ package com.yang.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
+//放在tomcat里运行的启动方式需要继承SpringBootServletInitializer
 @SpringBootApplication
-public class CodeApplication {
+public class CodeApplication extends SpringBootServletInitializer {
 
+    //重写configure方法
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+        return springApplicationBuilder.sources(CodeApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(CodeApplication.class, args);
     }
